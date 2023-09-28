1a6e2fddbe149f247a4437aa278db3b556822937
Sovit Khatri <shovitxettrie819@gmail.com>
2023-09-28 22:49:04 +0530

    Completed

---
+++ frontend/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className='text-center'>Order It @Copyright2023-2024, All rights Reserved</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
